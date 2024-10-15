b658ec3115d068daa824b4d339e9782572053227
Slavko Mihajlovic <143391636+dynimorius@users.noreply.github.com>
2024-10-15 15:36:23 +0200

    Setup the test file for show validation directive

--- projects/ngx-validator-pack/src/lib/directives/show-validation.directive.spec.ts
+++ projects/ngx-validator-pack/src/lib/directives/show-validation.directive.spec.ts
@@ -1,8 +1,23 @@
-import { ShowValidationDirective } from './show-validation.directive';
+import { NgControl } from "@angular/forms";
+import { ShowValidationDirective } from "./show-validation.directive";
+import { ElementRef, Renderer2 } from "@angular/core";
+import { TestBed } from "@angular/core/testing";
 
-describe('ShowValidationDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ShowValidationDirective();
+describe("ShowValidationDirective", () => {
+  TestBed.configureTestingModule({
+    providers: [ElementRef, Renderer2, NgControl],
+  });
+
+  let elementRef: ElementRef;
+  let renderer: Renderer2;
+  let control: NgControl;
+
+  it("should create an instance", () => {
+    const directive = new ShowValidationDirective(
+      elementRef,
+      renderer,
+      control
+    );
     expect(directive).toBeTruthy();
   });
 });
